bdbbc5642d98fa2f1f49ca789d0a3f700087997c
Charles Titus <ctitus@bnl.gov>
2024-08-23 22:03:45 -0400

    Initial commit of docker-compose compatible NBS Pods

---
+++ config/ipython/profile_default/startup/00-startup.py
@@ -0,0 +1,14 @@
+import nslsii
+from nbs_bl.configuration import load_and_configure_everything
+
+load_and_configure_everything()
+
+nslsii.configure_base(
+    get_ipython().user_ns, "nbs", publish_documents_with_kafka=True, bec=False
+)
+
+RE(psh10.open())
+RE(psh7.open())
+manipz.velocity.set(100)
+RE(mv(manipz, 464))
+manipz.velocity.set(1)
---
+++ images/build_bluesky_image.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/bash
+set -e
+set -o xtrace
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+version="0.0.1"
+container=$(buildah from conda-base)
+buildah run $container -- conda install -yc conda-forge pyepics
+buildah run $container -- pip3 install --pre databroker[all]
+buildah run $container -- pip3 install bluesky
+buildah run $container -- pip3 install nslsii tiled[all]
+buildah run $container -- pip3 install git+https://github.com/bluesky/bluesky-adaptive.git@main#egg=bluesky-adaptive
+buildah run $container -- pip3 install git+https://github.com/bluesky/bluesky-queueserver.git@main#egg=bluesky-queueserver
+
+buildah unmount $container
+
+buildah commit $container bluesky:latest
+buildah commit $container bluesky:$version
+
+buildah rm $container
