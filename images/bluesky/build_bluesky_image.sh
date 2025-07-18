63ee59f1afc51cad11e5b9c1cf3e506ae77c3333
Charles Titus <charles.titus@nist.gov>
2025-07-18 15:05:39 -0400

    Updating for no conda

--- images/bluesky/build_bluesky_image.sh
+++ images/bluesky/build_bluesky_image.sh
@@ -3,8 +3,9 @@ set -e
 set -o xtrace
 
 version="0.0.1"
-container=$(buildah from conda-base)
-buildah run $container -- conda install -yc conda-forge pyepics
+container=$(buildah from fedora)
+buildah run $container -- dnf install -y python3 python3-pip
+buildah run $container -- pip3 install pyepics
 buildah run $container -- pip3 install --pre databroker[all]
 buildah run $container -- pip3 install bluesky
 buildah run $container -- pip3 install nslsii tiled[all]
