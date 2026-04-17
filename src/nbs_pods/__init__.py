75be445e5b895706f29ef970c4212f481e4f8b47
Charles Titus <ctitus@bnl.gov>
2026-04-17 14:41:25 -0400

    Updating build system to hatch

--- src/nbs_pods/__init__.py
+++ src/nbs_pods/__init__.py
@@ -1,3 +1,8 @@
 """NBS Pods - Containerized NBS services management."""
 
-__version__ = "0.1.0"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("nbs-pods")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
